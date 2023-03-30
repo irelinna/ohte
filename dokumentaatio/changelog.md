c84d7076a52f62f23d39b778f56ad3cf69b82b53
Irene Linnanen <irene.linnanen@hotmail.com>
2023-03-30 16:51:48 +0300

    lisää alikansioita ja __init__.py tiedostot, changelog.md, tehty invoke-tehtävät

---
+++ dokumentaatio/changelog.md
@@ -0,0 +1,5 @@
+## Viikko 1-2
+Projektin suunnittelua
+
+## Viikko 3
+- Tähän mitä tällä viikolla tulee tehtyä
---
+++ tasks.py
@@ -0,0 +1,21 @@
+from invoke import task
+
+#poetry run invoke start
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py", pty=True)
+
+#poetry run invoke test
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+
+#poetry run invoke coverage-report
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
