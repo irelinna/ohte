3173e7b09340fe9ed55880af53045e012446020f
Irene Linnanen <irene.linnanen@hotmail.com>
2023-04-06 14:50:29 +0300

    .env made, build and config edited

---
+++ src/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+dirname = os.path.dirname(__file__)
+
+try:
+    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
+except FileNotFoundError:
+    pass
+
+ITEMS_FILENAME = os.getenv("ITEMS_FILENAME") or "items.csv"
+ITEMS_FILE_PATH = os.path.join(dirname, "..", "data", ITEMS_FILENAME)
+
+LISTS_FILENAME = os.getenv("LISTS_FILENAME") or "lists.csv"
+LISTS_FILE_PATH = os.path.join(dirname, "..", "data", LISTS_FILENAME)
+
+DATABASE_FILENAME = os.getenv("DATABASE_FILENAME") or "database.sqlite"
+DATABASE_FILE_PATH = os.path.join(dirname, "..", "data", DATABASE_FILENAME)
