9e5e94c3e2c11d7c7bb8a15464e81e72f3041598
Irene Linnanen <irene.linnanen@hotmail.com>
2023-05-02 20:59:44 +0300

    tasks.py päivitetty ja pylint lisätty

--- tasks.py
+++ tasks.py
@@ -1,24 +1,24 @@
 from invoke import task
-# pty=True taken away bc windows, will add back if needed
+
 
 #poetry run invoke start
 @task
 def start(ctx):
-    ctx.run("python3 src/index.py")
+    ctx.run("python3 src/index.py", pty=True)
 
 #poetry run invoke test
 @task
 def test(ctx):
-    ctx.run("pytest src")
+    ctx.run("pytest src", pty=True)
 
 #poetry run invoke coverage-report
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest")
+    ctx.run("coverage run --branch -m pytest", pty=True)
 
 @task(coverage)
 def coverage_report(ctx):
-    ctx.run("coverage html")
+    ctx.run("coverage html", pty=True)
 
 #poetry run invoke lint
 
