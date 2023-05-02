013515bf20768bdfe31db759e13df540e428fd66
Irene Linnanen <irene.linnanen@hotmail.com>
2023-05-02 21:00:19 +0300

    ui päivitetty

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,31 +1,31 @@
-from logic import app_methods
-from console_io import ConsoleIO
+from logic.app_methods import AppMethods
 
 ACTIONS = {
     "x": "x quit",
     "1": "1 login",
-    "2": "2 create new list",
+    "2": "2 create new user",
+    "3": "3 create new list",
     "4": "4 add item to list",
-    "5": "5 delete list",
-    "6": "6 logout",
-    "7": "7 create new user"
+    "5": "5 find list",
+    "6": "6 delete list",
+    "7": "7 logout"
 }
 
 
 class UserInterface:
     def __init__(self):
-        self._io = ConsoleIO()
-        self._methods = app_methods()
+        self._methods = AppMethods()
+    
 
     def start(self):
-        self._io.print("Grocery list app")
+        print("Grocery list app")
         self._print_instructions()
 
         while True:
-            action = self._io.read("action: ")
+            action = input("action: ")
 
             if not action in ACTIONS:
-                self._io.print("nonexistent action")
+                print("nonexistent action")
                 self._print_instructions()
                 continue
 
@@ -34,58 +34,69 @@ class UserInterface:
             elif action == "1":
                 self._login()
             elif action == "2":
-                self._create_item()
+                self._create_user()
             elif action == "3":
                 self._create_list()
             elif action == "4":
                 self._add_item_to_list()
             elif action == "5":
-                self._delete_list()
+                self._find_list()
             elif action == "6":
-                self._logout()
+                self._delete_list()
             elif action == "7":
-                self._create_user()
+                self._logout()  
         
 
     def _print_instructions(self):
         for value in ACTIONS.items():
             print(value)
 
+
     def _login(self):
-        username = self._io.print("username: ")
-        password = self._io.print("password: ")
+        username = print("username: ")
+        password = print("password: ")
         self._methods.login(username,password)
 
+
     def _create_user(self):
-        self._io.print("creating new user:")
-        username = self._io.read("username: ")
-        password = self._io.read("password: ")
+        print("creating new user:")
+        username = input("username: ")
+        password = input("password: ")
         self._methods.create_user(username,password)
 
-    def _create_item(self):
+
+    def _create_item(self, content, list_id):
         self._methods.create_item(content, list_id)
         
 
     def _create_list(self):
-        list_name = self._io.read("list name: ")
-
+        list_name = input("list name: ")
         self._methods.create_list(list_name)
 
-        item_name = self._io.read("write the items you want to add one at a time, write exit to finish list:")
+        list_id = self._methods.get_list_id(list_name)
+
+        item_name = input("write the items you want to add one at a time, write exit to finish list:")
 
         while item_name != 'exit':
-            self._methods.create_item(item_name)
+            self._methods.create_item(item_name, list_id)
 
-        
+    def _find_list(self):
+        list_name = input("which list would you like to see:")
+
+        self._methods.find_list_by_name(list_name)
+
+
+
+    def _delete_list(self):
+        list_name = input("which list would you like removed: ")
+        self._methods.delete_list(list_name)
+
 
 
     def _add_item_to_list(self):
-        item = self._io.read("which item would you like to add?:")
-        list = self._io.read("which list would you like to add it to?:")
+        item = input("which item would you like to add?:")
+        list = input("which list would you like to add it to?:")
         self._methods.add_item_to_list(item,list)
 
-    def logout(self):
+    def _logout(self):
         self._methods.logout()
-
-
-    # more todo
